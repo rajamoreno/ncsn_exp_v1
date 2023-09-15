1a8df9e5ccbd187ae4fb342943bccca1f660e8f8
rajamoreno <rajamehtamoreno@gmail.com>
2023-09-15 15:54:03 -0400

    large updates to introduction, wrote networkInitializer (to be connected to rest of code in next update)

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 Repository for code supporting network centrality and social norms research with Professor Jennifer Dannals.
 
 # description
-We're trying to set up a game played in a network parameterized by user input.
+We're trying to set up a game played in a network parameterized by user input to measure the effect of network centrality on a participant's perception of social norms.
 
 # running notes for maintenance
 
-to start server: empirica
-to reset local storage: rm .empirica/local/tajriba.json
+* to start server: empirica
+* to reset local storage: rm .empirica/local/tajriba.json
 
 # edit to check compliance with new personal access token
--- client/src/intro-exit/Introduction.jsx
+++ client/src/intro-exit/Introduction.jsx
@@ -1,17 +1,44 @@
 import React from "react";
 import { Button } from "../components/Button";
+import { usePlayer } from "@empirica/core/player/classic/react";
 
 export function Introduction({ next }) {
+
+  const player = usePlayer();
+
+  function onClick(generosity) {
+    console.log("you chose to contribute:", generosity);
+    player.set("generosity", generosity);
+    // player.set("submit", true);
+}
+
   return (
     <div className="mt-3 sm:mt-5 p-20">
       <h3 className="text-lg leading-6 font-medium text-gray-900">
-        Instruction One
+        Instructions and Introductory Question
       </h3>
       <div className="mt-2 mb-6">
         <p className="text-sm text-gray-500">
-          The INTRODUCTION/INSTRUCTIONS for the experiment will go here. We can format this however we'd like. The quick brown fox jumps over the lazy dog.
+          The INSTRUCTIONS for the game go here.
+          - instructions for playing the game
+          - instructions for filling out the first question -- how generous are they?
+          - once you fill out this question, it sets your "generosity" parameter as a player.
+          - "generosity" is trait a for the purposes of network initialization
         </p>
       </div>
+        <div className="flex w-sw justify-center">
+            <Button handleClick={() => onClick(0)}>0</Button>
+            <Button handleClick={() => onClick(1)}>1</Button>
+            <Button handleClick={() => onClick(2)}>2</Button>
+            <Button handleClick={() => onClick(3)}>3</Button>
+            <Button handleClick={() => onClick(4)}>4</Button>
+            <Button handleClick={() => onClick(5)}>5</Button>
+            <Button handleClick={() => onClick(6)}>6</Button>
+            <Button handleClick={() => onClick(7)}>7</Button>
+            <Button handleClick={() => onClick(8)}>8</Button>
+            <Button handleClick={() => onClick(9)}>9</Button>
+            <Button handleClick={() => onClick(10)}>10</Button>
+        </div>
       <Button handleClick={next} autoFocus>
         <p>Next</p>
       </Button>
