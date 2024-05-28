624b950fc670c531d2b3a87aab823c328934ffbc
rajamoreno <rajamehtamoreno@gmail.com>
2024-05-27 17:00:52 -0700

    Updated Introduction.jsx to reflect new experiment parameters

--- client/src/intro-exit/Introduction.jsx
+++ client/src/intro-exit/Introduction.jsx
@@ -10,39 +10,63 @@ export function Introduction({ next }) {
     console.log("Player Pressed Button: ", generosity);
     // sets this as the player's most recent contribution 
     player.set("lastContribution", generosity);
-    // scales the player's button press to a value in the range [0, 0.1, ... 0.9, 1]
-    const traitA = generosity / 10;
+    // scales the player's contribution to their traitA parameter like so:
+    //  0   -> 0.1
+    //  25  -> 0.3
+    //  50  -> 0.5
+    //  75  -> 0.7
+    //  100 -> 0.9
+    let traitA;
+    switch(generosity) {
+        case 0:
+            traitA = 0.1;
+            break;
+        case 25:
+            traitA = 0.3;
+            break;
+        case 50:
+            traitA = 0.5;
+            break;
+        case 75:
+            traitA = 0.7;
+            break;
+        case 100:
+            traitA = 0.9;
+            break;
+        default:
+            traitA = 0.0; // Default value in case something unexpected happens
+    }
+    console.log("Player's 'traitA' is: ", traitA);
     // sets this as traitA for access when constructing the game network.
     player.set("traitA", traitA);
     // player.set("submit", true);
-    console.log("Player's 'traitA' is: ", traitA);
     next();
   }
 
   return (
     <div className="mt-3 sm:mt-5 p-20">
       <h3 className="text-lg leading-6 font-medium text-gray-900">
-        Instructions and Introductory Question
+        Welcome to the Group Decision Making Game! 
       </h3>
       <div className="mt-2 mb-6">
-        <p> The INSTRUCTIONS for the game go here. </p>
-        <p> - instructions for playing the game </p>
-        <p> - instructions for filling out the first question -- how generous are they? </p>
-        <p> - once you fill out this question, it sets your "generosity" parameter as a player. </p>
-        <p> - "generosity" is trait a for the purposes of network initialization </p>
+        <h4 className="text-lg leading-6 font-medium text-gray-900"> 
+          Instructions: 
+        </h4>
+        <p>Everyone playing the game begins each Round with 100 tokens.  The game proceeds as follows:</p>
+        <ol>
+          <li>First, all players are given the option to contribute any number of these tokens (in increments of 25) to the collective pot.  Any tokens not contributed are kept by the participant in their private bank.  All players are given 20 seconds to make their decision.</li>
+          <li>Then, all tokens in the collective pot are doubled by the experimenter.</li>
+          <li>Finally, the collective pot (now twice the original size) is evenly divided amongst all the participants and deposited in their private bank, regardless of whether or not they donated any tokens.</li>
+        </ol>
+        <p>The game has five rounds total.  At the beginning of each round all players get another 100 tokens to play with.  At the end of all 5 rounds, any tokens in one’s private pot are converted to a monetary bonus at a rate of 100 tokens = $0.50.</p>
+        <p><strong>Round 1 has not yet begun.  Before it starts, please indicate using the buttons below how many tokens you wish to contribute to the collective pot.  You will then enter the game, learn about your fellow players and begin Round 1.</strong></p>
       </div>
       <div className="flex w-sw justify-center">
         <Button handleClick={() => onClick(0)}>0</Button>
-        <Button handleClick={() => onClick(1)}>1</Button>
-        <Button handleClick={() => onClick(2)}>2</Button>
-        <Button handleClick={() => onClick(3)}>3</Button>
-        <Button handleClick={() => onClick(4)}>4</Button>
-        <Button handleClick={() => onClick(5)}>5</Button>
-        <Button handleClick={() => onClick(6)}>6</Button>
-        <Button handleClick={() => onClick(7)}>7</Button>
-        <Button handleClick={() => onClick(8)}>8</Button>
-        <Button handleClick={() => onClick(9)}>9</Button>
-        <Button handleClick={() => onClick(10)}>10</Button>
+        <Button handleClick={() => onClick(25)}>25</Button>
+        <Button handleClick={() => onClick(50)}>50</Button>
+        <Button handleClick={() => onClick(75)}>75</Button>
+        <Button handleClick={() => onClick(100)}>100</Button>
       </div>
     </div>
   );
